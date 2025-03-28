1c8439c2a5b404e7dddcd6d4801e1fb3b3b6c9d5
Sarah783 <tranthuythanhtruc2003@gmail.com>
2025-03-28 23:53:41 +0700

    add

--- index.php
+++ index.php
@@ -6,29 +6,27 @@
     <title>THE ORDINARY</title>
 
     <!-- fontawesome -->
-    <link rel="icon" href="https://fontawesome.com/">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Alatsi&family=Gidole&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
-    <link rel="stylesheet" href="public/css/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <!-- Header -->
+    <div class="header">
+        <div class="header-top">
+            <div class="header-slide">
+                <button class="header-icon">
+                    <i class="fa-solid fa-chevron-left" style="color: black;"></i>
+                    <span class="sr-only">Previous</span>
+                </button>
+            </div>
+        </div>
+    </div>
+
+
 </body>
 </html>
-
-<?php
-
-
-    include 'text.php';
-
-
-
-    // require('Model/Database.php');
-	// $db = new Database;
-	// $db->connect();
-
-	// $db->closeDatabase();
-?>
