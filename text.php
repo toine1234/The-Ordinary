48a479c52bb647c75e6b6af40d38b4d9b93da4f1
Sarah783 <tranthuythanhtruc2003@gmail.com>
2025-03-28 09:57:23 +0700

    add

---
+++ text.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+        <!-- fontawesome -->
+        <link rel="icon" href="https://fontawesome.com/">
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alatsi&family=Gidole&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    hello
+</body>
+</html>
