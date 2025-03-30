eeeb10ce3232fd7b7be72a41c992634cb39db9db
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-03-30 16:40:15 +0700

    header-top, brand-nav, menu ( Layout )

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,32 +11,86 @@
     <!-- Google Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Alatsi&family=Gidole&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Alatsi&family=Gidole&family=Roboto:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+        rel="stylesheet">
 
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <!-- Header -->
-    <div class="header">
-        <div class="header-top">
-            <div class="header-slide">
-                <button class="header-icon-previous">
-                    <i class="fa-solid fa-chevron-left" style="color: black;"></i>
-                    <span class="sr-only">Previous</span>
-                </button>
+    <div class="main-nav">
+        <div class="header">
+            <div class="header-top">
+                <div class="header-slide">
+                    <button class="header-icon-previous">
+                        <i class="fa-solid fa-chevron-left"></i>
+                        <span class="sr-only">Previous</span>
+                    </button>
 
-                <div class="header-promo-slider">
-                    
+                    <div class="header-promo-slider">
+
+                    </div>
+
+                    <button class="header-icon-next">
+                        <i class="fa-solid fa-chevron-right"></i>
+                        <span class="sr-only">Next</span>
+                    </button>
                 </div>
+                <div class="icon-header">
+                    <button class="icon-header-btn">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </button>
+                    <button class="icon-header-btn">
+                        <i class="fa-solid fa-user"></i>
+                    </button>
+                    <button class="icon-header-btn">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="brand-nav">
+            <ul class="brand-list">
+                <li class="brand-item active-brand">
+                    <a>
+                        <img class="img-brand" src="./public/images/logoTO.png">
+                        <p>THE <br> ORDINARY</p>
+                    </a>
+                </li>
+                <li class="brand-item">
+                    <a>
 
-                <button class="header-icon-next">
-                <i class="fa-solid fa-chevron-right" style="color: black;"></i>
-                    <span class="sr-only">Next</span>
-                </button>
+                        <p>NIOD</p>
+                    </a>
+                </li>
+                <li class="brand-item">
+                    <a>
+
+                        <p>LOOPHA</p>
+                    </a>
+                </li>
+                <li class="brand-item">
+                    <a>
+
+                        <p>DECIEM</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="navbar-expand">
+            <div class="menu-group">
+                <ul class="list-menu">
+                    <li class="menu-item">New</li>
+                    <li class="menu-item">Bestsellers</li>
+                    <li class="menu-item">Skincare</li>
+                </ul>
             </div>
         </div>
     </div>
 
 
 </body>
+
 </html>
