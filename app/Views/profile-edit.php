da70009087fc34283b16a083a23d60881f698af5
Sarah783 <tranthuythanhtruc2003@gmail.com>
2025-04-17 16:34:02 +0700

    update profileEdit_view

--- app/Views/profile-edit.php
+++ app/Views/profile-edit.php
@@ -1,3 +1,29 @@
-<div class="page-profile-edit-container">
-    
+<div class="page-profile-edit-container" id="edit-profile">
+    <div class="profile-edit-breadcrumb">
+        <ul class="nav-profile-edit">
+            <li><a href="/The-Ordinary/account" style="color: gray; margin-right: 10px;">Account</a></li>&rsaquo;
+            <li><a style="margin-left: 10px;" href="#edit-profile">Edit Profile</a></li>
+        </ul>
+    </div>
+
+    <div class="is-divider-login"></div>
+
+    <div class="profile-edit-wrapper">
+        <h3>EDIT PROFILE</h3>
+        <form method="post">
+            <input class="signup-input" type="text" name="name" placeholder="Full name" >
+            <input class="signup-input" type="text" name="phone" placeholder="Phone" >
+            <input class="signup-input" type="email" name="email" placeholder="Email" required>
+            <div class="input-group">
+                <input type="password" name="password" placeholder="Password" required>
+                <i class="fa-regular fa-eye-slash"></i>
+            </div>
+            <div class="input-group">
+                <input type="password" name="confirm-password" placeholder="Confirm Password" required>
+                <i class="fa-regular fa-eye-slash"></i>
+            </div>
+            <button type="submit" class="btn-profile-edit">SAVE CHANGES</button>
+        </form>
+    </div>
+
 </div>
