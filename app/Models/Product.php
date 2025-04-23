3f4ff6352ebf2f9f42ede09cc340bd0e8a7f0564
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-04-23 10:11:50 +0700

    up lib

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,9 +18,9 @@ class Product {
     public static function getProductsById($id) {
         $database = new Database();
         $db = $database->getConnection();
-        $query = "SELECT * FROM san_pham WHERE ID_San_Pham = $id";
+        $query = "SELECT * FROM san_pham WHERE ID_San_Pham = :id";
         $stmt = $db->prepare($query);
-        $stmt->execute();
+        $stmt->execute([":id"=>trim($id)]);
         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
     }
     
