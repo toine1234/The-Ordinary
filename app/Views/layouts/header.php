2bf8f19ea3743d769e1502f5ee7070f05107ad3b
Sarah783 <tranthuythanhtruc2003@gmail.com>
2025-05-17 20:50:19 +0700

    Responsive of pageHome on the moblie is finished

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -18,6 +18,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <link rel="stylesheet" href="public/assets/css/style.css?v=<?= time() ?>" />
+    <link rel="stylesheet" href="public/assets/css/responsive.css?v=<?= time() ?>" />
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" />
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link
